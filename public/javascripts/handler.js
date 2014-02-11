e49f9f17fbc5d849cf7e47c87d34001b5501420c
Chadwick Jones <jonescd@rose-hulman.edu>
2014-02-11 08:39:48 -0500

    Finally styled it correctly.  made the player at the bottom.  made the navbar.

--- public/javascripts/handler.js
+++ public/javascripts/handler.js
@@ -8,23 +8,30 @@
 'use strict';
 
 // Create an instance
-var ws = [];
+
+var ws;
+var isplaying = false;
+var ismuted = false;
+var willRepeat = false;
 
 var loadMusic = function (url) {
 
 
 	var wavesurfer = Object.create(WaveSurfer);
 
+	ws = wavesurfer;
+
 
 // Init & load audio file
 	document.addEventListener('DOMContentLoaded', function () {
 		var options = {
 			container: document.querySelector('.waveform'),
-			waveColor: 'rgb(104, 165, 165)',
-			progressColor: 'rgb(81, 134, 134)',
+			waveColor: '#818181',
+			progressColor: '#fff',
 			loaderColor: 'rgb(81, 134, 134)',
-			cursorColor: 'navy',
-			markerWidth: 2
+			cursorColor: '#c1333f',
+			height: 50,
+			markerWidth: 1
 		};
 
 		if (location.search.match('scroll')) {
@@ -39,6 +46,7 @@ var loadMusic = function (url) {
 		/* Progress bar */
 		var progressDiv = document.querySelector('.progress-bar-outer');
 		var progressBar = progressDiv.querySelector('.progress-bar');
+		var pageImg = document.querySelector('#play-pause');
 		wavesurfer.on('loading', function (percent, xhr) {
 			progressDiv.style.display = 'block';
 			progressBar.style.width = percent + '%';
@@ -49,6 +57,17 @@ var loadMusic = function (url) {
 		wavesurfer.on('destroy', function () {
 			progressDiv.style.display = 'none';
 		});
+
+		
+		// wavesurfer.on('play', function () {	
+		// 	console.log('playing');		
+		// 	pageImg.src = '/images/pauseButton.svg';
+		// });
+
+		// wavesurfer.on('finish', function () {
+		// 	console.log('finished');
+		// 	pageImg.src = '/images/playButton.svg';
+		// });
 
 		// Init
 		wavesurfer.init(options);
@@ -61,8 +80,16 @@ var loadMusic = function (url) {
 // Play at once when ready
 // Won't work on iOS until you touch the page
 	wavesurfer.on('ready', function () {
+		// $("endTime").text('2:33');
 //    wavesurfer.play();
 	});
+
+	wavesurfer.on('finish', function () {
+			if(willRepeat){
+				ws.stop();
+				ws.play();
+			}
+		});
 
 // Bind buttons and keypresses
 
@@ -70,6 +97,13 @@ var loadMusic = function (url) {
 		var eventHandlers = {
 			'play': function () {
 				wavesurfer.playPause();
+				if(!isplaying){
+					isplaying = true;
+					document.images['play-pause'].src = '/images/pauseButton.svg';
+				} else {
+					isplaying = false;
+					document.images['play-pause'].src = '/images/playButton.svg';
+				}
 			},
 
 			'green-mark': function () {
@@ -102,8 +136,8 @@ var loadMusic = function (url) {
 		document.addEventListener('keydown', function (e) {
 			var map = {
 				32: 'play',       // space
-				38: 'green-mark', // up
-				40: 'red-mark',   // down
+				// 38: 'green-mark', // up
+				// 40: 'red-mark',   // down
 				37: 'back',       // left
 				39: 'forth'       // right
 			};
@@ -143,8 +177,50 @@ var loadMusic = function (url) {
 		console.error(err);
 	});
 
-	ws.push(wavesurfer);
 };
+
+$(document).on('click', '#play-pause', function(){
+	ws.playPause();
+	if(!isplaying){
+		isplaying = true;
+		document.images['play-pause'].src = '/images/pauseButton.svg';
+	} else {
+		isplaying = false;
+		document.images['play-pause'].src = '/images/playButton.svg';
+	}
+});
+
+$(document).on('click', '#skip-left', function(){
+	ws.playPause();
+	if(!isplaying){
+		ws.stop();
+	} else {
+		ws.stop();
+		ws.play();
+	}
+});
+
+$(document).on('click', '#mute', function(){
+	ws.toggleMute();
+	if(!ismuted){
+		ismuted = true;
+		document.images['mute'].src = '/images/volumeMute.svg';
+	} else {
+		ismuted = false;
+		document.images['mute'].src = '/images/volumeFull.svg';
+	}
+});
+
+$(document).on('click', '#repeat', function(){
+	if(!willRepeat){
+		willRepeat = true;
+		document.images['repeat'].src = '/images/repeat.svg';
+	} else {
+		willRepeat = false;
+		document.images['repeat'].src = '/images/repeatFalse.svg';
+	}
+});
+
 
 
 
