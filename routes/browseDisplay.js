2b40463dbf7ac7f4b8dfacdd39a31e4c435e22ca
root <root@spotifeh.csse.rose-hulman.edu>
2014-02-16 15:33:16 -0500

    Database access, the right way

--- routes/browseDisplay.js
+++ routes/browseDisplay.js
@@ -4,18 +4,23 @@ var connection = mysql.createConnection({
 	  host     : 'spotifeh.csse.rose-hulman.edu',
 	  user     : 'spotifeh_admin',
 	  password : 'Pekaege7',
+	  database : 'spotifeh'
 });
 
-exports.artists = function(req,res){
-	connection.connect(function(err) {
-	  res.send(err);//'hello world, bitches');
+connection.connect(function(err) {
+	 // res.send(err); // 'hello world, bitches');
 	});
 
-	// connection.query('select id, code, name from Continent', function (err, rows, fields) {
-	//     // close connection first
-	//     closeConnection(connection);
-	//     // done: call callback with results
-	//     cb(err, rows);
- //  	});
+exports.artists = function(req,res){
+	
+
+	connection.query('select Name from Artist', function (err, rows, fields) {
+	     // close connection first
+	    // closeConnection(connection);
+	//     // done: call callback with results	     //cb(err, rows);
+	  if (err) throw err;
+	  res.send(rows[0]);
+ });
+//	connection.end();
 }
 	
